c73c8768075f06c804b1ad931216666c66f6e745
pedrosant <roberto19santana@gmail.com>
2020-02-08 00:55:35 -0300

    Inserir vértices, adjacências e imprmir matriz de adjacências

---
+++ src/grafos/Principal.java
@@ -0,0 +1,47 @@
+package grafos;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Roberto Borges
+ */
+    public class Principal {
+   
+    public static void main(String[] args) {
+        Adjacencia adj;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o número de vertices de seu grafo: ");
+        int n = scan.nextInt();
+        int matrizAdj[][] = new int [n][n];
+        
+        for (int i = 0; i < matrizAdj.length; i++) {
+            for (int j = 0; j < matrizAdj.length; j++) {
+                System.out.print(matrizAdj[i][j]+ " | ");
+            }
+            System.out.println("");
+        }
+        
+        System.out.println("Adicionar adjacências (s/n)");
+        String escolha = scan.next();
+        do {
+            
+            if (escolha.equals("s")) {
+                System.out.println("Digite o primeiro vertice adjacente");
+            int v1 = scan.nextInt();
+                System.out.println("Digite o primeiro vertice adjacente");
+            int v2 = scan.nextInt();
+                matrizAdj[v1][v2] = 1;
+                matrizAdj[v2][v1] = 1;
+                System.out.println("Agrescetar outra adjaceencia (s/n) ?");
+                escolha = scan.next();
+        }}while(escolha.equals("s"));        
+        System.out.println("********MATRIZ DE ADJACÊNCIA*********");
+        for (int i = 0; i < matrizAdj.length; i++) {
+            for (int j = 0; j < matrizAdj.length; j++) {
+                System.out.print(matrizAdj[i][j]+ " | ");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
